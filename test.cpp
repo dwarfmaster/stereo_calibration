50ee2cbb41fd6a0af2c1bbc991f65e9a8da26030
lucas8 <luc.linux@mailoo.org>
2014-01-19 11:56:15 +0100

    Updated test main function.

--- test.cpp
+++ test.cpp
@@ -17,27 +17,9 @@ int main()
         iss >> left >> right;
 
         std::cout << "Remapping " << left << " and " << right << std::endl;
-        cv::Mat imgml = cv::imread(left,  cv::IMREAD_GRAYSCALE);
-        cv::Mat imgmr = cv::imread(right, cv::IMREAD_GRAYSCALE);
-        calib.transform(imgml, imgmr);
-
-        cv::Mat imgpl, imgpr;
-        cv::cvtColor(imgml, imgpl, CV_GRAY2BGR);
-        cv::cvtColor(imgmr, imgpr, CV_GRAY2BGR);
-        for(size_t i = 0; i < 480; i += 16) {
-            cv::line(imgpl, cv::Point(0, i),
-                    cv::Point(640, i), cv::Scalar(0, 255, 0));
-            cv::line(imgpr, cv::Point(0, i),
-                    cv::Point(640, i), cv::Scalar(0, 255, 0));
-        }
-        cv::imshow("Left",  imgpl);
-        cv::imshow("Right", imgpr);
-        
-        // Saving
-        std::string path = "trans/" + left;
-        cv::imwrite(path, imgml);
-        path = "trans/" + right;
-        cv::imwrite(path, imgmr);
+        cv::Mat imgl = cv::imread(left,  cv::IMREAD_GRAYSCALE);
+        cv::Mat imgr = cv::imread(right, cv::IMREAD_GRAYSCALE);
+        calib.process(imgl, imgr);
 
         while((char)cv::waitKey() != ' ');
     }
